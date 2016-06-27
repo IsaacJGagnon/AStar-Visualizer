c2c70c198f87c41e71cb84712b84dad37d1b8a3e
IsaacJGagnon <isaacjgagnon@gmail.com>
2016-06-26 20:12:50 -0400

    Update Node.java

--- Node.java
+++ Node.java
@@ -1,3 +1,13 @@
+/**********************************
+ * Created by Isaac Gagnon 6/11/2016
+ * 
+ * Nodes represent the generated movement possibilities. These are put
+ * into a priority queue and expanded one by one until the goal is
+ * reached.
+ * 
+ * Last updated 6/18/2016
+ **********************************/
+
 import java.awt.Point;
 import java.lang.Comparable;
 
@@ -6,64 +16,94 @@ public class Node implements Comparable<Node>{
 	private double fVal;
 	private double gVal;
 	private Node parent;
-	private String moveDir;
+	private int gVisited;
 	
 	Node(int x, int y){
 		location = new Point(x,y);
 		fVal = 0;
 		gVal = 0;
 		parent = null;
-		moveDir = "";
+		gVisited = 0;
 	}
 	
-	Node(int x, int y, double f, double g, Node p, String s){
+	Node(int x, int y, int c, double f, double g, Node p){
 		location = new Point(x,y);
 		fVal = f;
 		gVal = g;
 		parent = p;
-		moveDir = s;
+		gVisited = c;
 	}
 	
+	/**
+	 * Returns the "location" of the node as a Point
+	 **/
 	public Point getLoc(){
 		return location;
 	}
 	
+	/**
+	 * Returns the number of goals reached by this point in the path
+	 **/
+	public int getVisited(){
+		return gVisited;
+	}
+	
+	/**
+	 * Returns the f value of the node. f(n) = g(n) + h(n) where g is
+	 * the cost to get to the current node, and h is the heuristic
+	 * estimate to get to the goal.
+	 **/
 	public double getF(){
 		return fVal;
 	}
 	
+	/**
+	 * Returns the g value of the node, where g is the cost to get to
+	 * the current node.
+	 **/
 	public double getG(){
 		return gVal;
 	}
 	
+	/**
+	 * Returns the node that expanded this node. Used to color the path
+	 * after the goal has been reached.
+	 **/
 	public Node getParent(){
 		return parent;
 	}
 	
-	public String getMove(){
-		return moveDir;
-	}
-	
+	/**
+	 * Compares this node to another node for precedence. The node with
+	 * more goals visited is given precedence. Then the node with the
+	 * lower f value. If two nodes have the same f value, the node with 
+	 * the higher g value is given precedence.
+	 **/
 	@Override
 	public int compareTo(Node o){
-		double fDif = fVal - o.getF();
-		if(fDif == 0){
-			double gDif = o.getG() - gVal;
-			if(gDif > 0){
-				return 1;
-			} else if(gDif < 0){
-				return -1;
+		int visDif = o.getVisited() - gVisited;
+		if(visDif == 0){
+			double fDif = fVal - o.getF();
+			if(fDif == 0){
+				double gDif = o.getG() - gVal;
+				if(gDif > 0){
+					return 1;
+				} else if(gDif < 0){
+					return -1;
+				} else {
+					return 0;
+				}
 			} else {
-				return 0;
+				if(fDif > 0){
+					return 1;
+				} else if(fDif < 0){
+					return -1;
+				} else {
+					return 0;
+				}
 			}
 		} else {
-			if(fDif > 0){
-				return 1;
-			} else if(fDif < 0){
-				return -1;
-			} else {
-				return 0;
-			}
+			return visDif;
 		}
 	}
 }
