86c8f87de0ab89222c30217b609327e80d0cccbf
IsaacJGagnon <isaacjgagnon@gmail.com>
2016-06-26 20:14:37 -0400

    Update Visualizer.java

--- Visualizer.java
+++ Visualizer.java
@@ -1,41 +1,51 @@
+/**********************************
+ * Created by Isaac Gagnon 6/11/2016
+ * 
+ * This is a simple program that scans the world (in the form of a
+ * 2D Tile array) and builds a corresponding display out of colored
+ * squares. It updates every time a node is expanded.
+ * 
+ * Last updated 6/18/2016
+ **********************************/
+
 import javax.swing.JPanel;
 import java.awt.Graphics;
 import java.awt.Color;
 import java.awt.Point;
 
 public class Visualizer extends JPanel{
-	private int[][] map;
-	private Point start;
-	private Point end;
+	private Tile[][] map;
 	
-	Visualizer(int[][] m, Point s, Point e){
+	Visualizer(Tile[][] m){
 		super();
 		setBackground(Color.white);
 		map = m;
-		start = s;
-		end = e;
 	}
 	
-	private void setColor(Graphics g, int n){
+	/**
+	 * Takes in an int and a graphics object, and sets the color of 
+	 * the graphics object according to the int.
+	 **/
+	private void updateColor(Graphics g, int n){
 		switch(n){
 			case 0: g.setColor(Color.gray);
 				break;
-			case 1: g.setColor(Color.yellow);
+			case 1: g.setColor(Color.orange);
 				break;
-			case 2: g.setColor(Color.green);
+			case 2: g.setColor(Color.magenta);
 				break;
 			case 3: g.setColor(Color.black);
 				break;
-			case 4: g.setColor(Color.magenta);
-				break;
-			case 5: g.setColor(Color.red);
-				break;
-			case 6: g.setColor(Color.blue);
+			case 4: g.setColor(Color.green);
 				break;
 			default: g.setColor(Color.gray);
 		}
 	}
 	
+	/**
+	 * Runs a double for loop over the representative 2D Tile array
+	 * and builds a display out of colored squares.
+	 **/
 	protected void paintComponent(Graphics g){
 		int width = getWidth();
 		int height = getHeight();
@@ -44,11 +54,13 @@ public class Visualizer extends JPanel{
 		
 		for(int i = 0; i<map.length; i++){
 			for(int j = 0; j<map[0].length; j++){
-				setColor(g,map[i][j]);
-				if((i == start.getY()) && (j == start.getX())){
+				//setColor(g,map[i][j]);
+				if(map[i][j].getSVal() == "@"){
 					g.setColor(Color.blue);
-				} else if((i == end.getY()) && (j == end.getX())){
+				} else if(map[i][j].getSVal() == "*"){
 					g.setColor(Color.red);
+				} else {
+					updateColor(g,map[i][j].getIVal());
 				}
 				g.fillRect(j*4, i*4, 4, 4);
 			}
